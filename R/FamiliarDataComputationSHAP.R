cfbacd0ccecf595e2d250ae7bee5b9ef653ede73
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2025-02-12 17:14:16 +0100

    Added implementation plan for SHAP computations.

--- R/FamiliarDataComputationSHAP.R
+++ R/FamiliarDataComputationSHAP.R
@@ -97,3 +97,42 @@ setMethod(
     return(NULL)
   }
 )
+
+
+# Step 1: Determine feature values that are to be sampled for determining SHAP
+# values.
+
+# Step 2: Determine the minimum sampleset X required to determine SHAP values.
+# The number of samples (n) is equal to the feature with the largest number of 
+# values (m_i) to sample. Feature values can be randomly ordered for each
+# feature. Features with m_i < n can randomly draw additional features.
+
+# NOTE: Is step 2 really necessary? We can also determine SHAP values for each
+# feature value, by randomly generating coalitions, keeping track of the
+# results, and using these in subsequent calculations.
+
+# Yes necessary -- however, we can still track which predictions have been made
+# for which generated feature sets. Depending on the current sample in the 
+# minimum sample set, these predictions represent different coalitions, which we
+# then get to use for free!
+
+# Step 3: Create coalition sets for coalitions with all but one off and all but
+# one on.
+
+# Step 4: Iterate samples in sampleset X. Generate samples corresponding to
+# coalitions for each sample. Concatenate generated samples and add to table
+# with previously generated samples X_gen.
+
+# Step 5: Select samples without existing predictions.
+
+# Step 6: Predict samples. Merge new predictions into existing predictions.
+
+# Step 7: Compute average predicted value (phi_0).
+
+# Step 8: For each sample in sampleset X, determine coalition represented by
+# each sample in X_gen. Compute kernel weight based on coalition. Compute SHAP
+# values by solving linear equation.
+
+# Step 9: Average SHAP value for each feature value.
+
+# Step 10: Determine convergence and repeat steps 4-9.
