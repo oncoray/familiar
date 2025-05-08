75cb87f1178ff68c8613d52dc306d88f49171303
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2025-05-08 14:20:32 +0200

    Update test to test multinomial data.

--- tests/testthat/test-export_shap.R
+++ tests/testthat/test-export_shap.R
@@ -7,6 +7,7 @@ debug_flag <- TRUE
 # Default
 results <- familiar:::test_export_specific(
   export_function = familiar:::export_shap,
+  outcome_type_available = "multinomial",
   data_element = "shap",
   create_novelty_detector = FALSE,
   debug = debug_flag
