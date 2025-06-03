ada0c3193c719247f538d273294d52040bed2796
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2025-06-03 17:40:54 +0200

    Add unit tests for summary plots.

--- tests/testthat/test-plot_shap_summary.R
+++ tests/testthat/test-plot_shap_summary.R
@@ -26,6 +26,7 @@ familiar:::test_plot_ordering(
 )
 
 
+# Test with single samples
 familiar:::test_plot_ordering(
   plot_function = familiar::plot_shap_summary,
   data_element = "shap",
@@ -35,6 +36,20 @@ familiar:::test_plot_ordering(
     "draw" = debug_flag
   ),
   use_single_sample = TRUE,
+  debug = debug_flag
+)
+
+
+# Test with absolute values.
+familiar:::test_plot_ordering(
+  plot_function = familiar::plot_shap_summary,
+  data_element = "shap",
+  evaluation_time = c(1.0, 2.0, 3.5),
+  plot_args = list(
+    "value_representation" = "abs",
+    "verbose" = FALSE,
+    "draw" = debug_flag
+  ),
   debug = debug_flag
 )
 
@@ -54,7 +69,7 @@ familiar:::test_plot_ordering(
   debug = debug_flag
 )
 
-# By specifying value_representation as abs_mean..
+# By specifying value_representation as abs_mean.
 familiar:::test_plot_ordering(
   plot_function = familiar::plot_shap_summary,
   data_element = "shap",
@@ -92,7 +107,30 @@ familiar:::test_plot_ordering(
   ),
   debug = debug_flag
 )
+
 # Box plot ---------------------------------------------------------------------
+familiar:::test_plot_ordering(
+  plot_function = familiar::plot_shap_summary,
+  data_element = "shap",
+  evaluation_time = c(1.0, 2.0, 3.5),
+  plot_args = list(
+    "plot_type" = "boxplot",
+    "verbose" = FALSE,
+    "draw" = debug_flag
+  ),
+  debug = debug_flag
+)
 
 
 # Violin plot ------------------------------------------------------------------
+familiar:::test_plot_ordering(
+  plot_function = familiar::plot_shap_summary,
+  data_element = "shap",
+  evaluation_time = c(1.0, 2.0, 3.5),
+  plot_args = list(
+    "plot_type" = "violinplot",
+    "verbose" = FALSE,
+    "draw" = debug_flag
+  ),
+  debug = debug_flag
+)
