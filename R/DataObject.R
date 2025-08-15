6d476980335ab0810c33449dea501c9f9f9985d7
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2025-08-15 18:00:44 +0200

    Add clarification for data loaded using an ensemble.
    Fixes an error with subsampling and internal validation.

--- R/DataObject.R
+++ R/DataObject.R
@@ -866,8 +866,16 @@ setMethod(
     
     # Check whether model data- and run-ids should be used.
     if (data@defer_to_model_data_and_run_id) {
+      # This forces data to be read at the ensemble level. Other pathways (e.g.
+      # predict) defer directly to the models, and reads data there.
       data@data_id <- object@data_id
       data@run_id <- object@run_id
+      
+      # Specific to ensemble models: if data should be read at the ensemble
+      # level, then validation should always be false. For external data,
+      # data is never forced to be read at the model level, and this attribute
+      # is not reset.
+      data@validation <- FALSE
     }
     
     # Read required features.
