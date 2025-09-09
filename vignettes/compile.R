b6f1f0450c8033b1a80efae3e936c89f0d27f26a
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2025-09-09 09:49:10 +0200

    Remove compilition of prospective use vignette.

--- vignettes/compile.R
+++ vignettes/compile.R
@@ -8,15 +8,6 @@ knitr::opts_chunk$set(fig.path = "eval_and_explain/")
 # See https://ropensci.org/blog/2019/12/08/precompute-vignettes/
 knitr::knit("vignettes/original/evaluation_and_explanation.Rmd", output = "vignettes/evaluation_and_explanation_precompiled.Rmd")
 
-
-
-# Restart the session to flush the temporary directories.
-knitr::opts_knit$set(root.dir = getwd())
-knitr::opts_knit$set(base.dir = "vignettes")
-knitr::opts_chunk$set(fig.path = "prospective_use/")
-
-knitr::knit("vignettes/original/prospective_use.Rmd", output = "vignettes/prospective_use_precompiled.Rmd")
-
 knitr::knit("vignettes/original/introduction.Rmd", output = "vignettes/introduction_precompiled.Rmd")
 knitr::knit("vignettes/original/variable_importance.Rmd", output = "vignettes/variable_importance_precompiled.Rmd")
 knitr::knit("vignettes/original/learners.Rmd", output = "vignettes/learners_precompiled.Rmd")
