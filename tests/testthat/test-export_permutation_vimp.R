c28175df22b0122640e6408a618a494b50a5d8c7
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-03-13 15:03:59 +0100

    Add test.

--- tests/testthat/test-export_permutation_vimp.R
+++ tests/testthat/test-export_permutation_vimp.R
@@ -5,18 +5,62 @@ testthat::skip_on_ci()
 
 debug_flag <- FALSE
 
-model <- familiar::train_familiar(
-  data = familiar:::test_create_good_data("binomial"),
-  vimp_method = "mim",
-  learner = "glm_logistic",
-  parallel = FALSE
-)
-
-
+# Without setting the number of important features.
 results <- familiar:::test_export_specific(
   export_function = familiar:::export_permutation_vimp,
   data_element = "permutation_vimp",
   outcome_type_available = "continuous",
   create_novelty_detector = FALSE,
   debug = debug_flag
+)
+
+testthat::test_that(
+  "all feature are assessed",
+  {
+    data <- results$continuous[[1L]]@data
+    features <- unique(data$feature)
+    testthat::expect_length(features, 6L)
+  }
+)
+
+
+# With setting the number of important features to 2.
+results <- familiar:::test_export_specific(
+  export_function = familiar:::export_permutation_vimp,
+  data_element = "permutation_vimp",
+  outcome_type_available = "continuous",
+  n_important_features = 2L,
+  create_novelty_detector = FALSE,
+  debug = debug_flag
+)
+
+testthat::test_that(
+  "the 2 most important features are assessed",
+  {
+    data <- results$continuous[[1L]]@data
+    features <- unique(data$feature)
+    testthat::expect_length(features, 2L)
+    testthat::expect_setequal(features, c("feature_1", "feature_2b"))
+  }
+)
+
+
+# With setting the number of important features to 1.
+results <- familiar:::test_export_specific(
+  export_function = familiar:::export_permutation_vimp,
+  data_element = "permutation_vimp",
+  outcome_type_available = "continuous",
+  n_important_features = 1L,
+  create_novelty_detector = FALSE,
+  debug = debug_flag
+)
+
+testthat::test_that(
+  "the most important feature is assessed",
+  {
+    data <- results$continuous[[1L]]@data
+    features <- unique(data$feature)
+    testthat::expect_length(features, 1L)
+    testthat::expect_setequal(features, "feature_1")
+  }
 )
