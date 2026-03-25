08bb286ca3684270b22fee98f9cb4c149bf01753
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-03-25 13:17:55 +0100

    Extend tests to use n_important_features.

--- tests/testthat/test-plot_shap_dependence.R
+++ tests/testthat/test-plot_shap_dependence.R
@@ -14,6 +14,23 @@ familiar:::test_plots(
   not_available_no_samples = FALSE
 )
 
+# The default limit for the number of important features exceeds the number of
+# features in the data, which means that no actual feature selection has 
+# to take place. To test all path ways, we set the number of important features
+# to one here. Because the test routine relies on "none" as variable importance
+# method, this triggers ad-hoc feature selection when computing the dataset.
+# This will fail if outcome data are absent, mostly absent, or singular.
+familiar:::test_plots(
+  plot_function = familiar::plot_shap_dependence,
+  not_available_all_prospective = TRUE,
+  not_available_mostly_prospective = TRUE,
+  not_available_single_sample = TRUE,
+  data_element = "shap",
+  shap_max_iterations = 10L,
+  n_important_features = 1L,
+  debug = debug_flag
+)
+
 # Test without interaction.
 familiar:::test_plots(
   plot_function = familiar::plot_shap_dependence,
