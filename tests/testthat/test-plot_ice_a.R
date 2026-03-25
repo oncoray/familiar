4d6318af94ea8ca04d214c0722c8e1450640acb4
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-03-25 09:14:17 +0100

    Add test to prevent automatically skipping fallback feature selection for ICE plots.

--- tests/testthat/test-plot_ice_a.R
+++ tests/testthat/test-plot_ice_a.R
@@ -11,3 +11,20 @@ familiar:::test_plots(
   data_element = "ice_data",
   debug = debug_flag
 )
+
+# The default limit for the number of important features exceeds the number of
+# features in the data, which means that no actual feature selection has 
+# to take place. To test all path ways, we set the number of important features
+# to one here. Because the test routine relies on "none" as variable importance
+# method, this triggers ad-hoc feature selection when computing the dataset.
+# This will fail if outcome data are absent, mostly absent, or singular.
+familiar:::test_plots(
+  plot_function = familiar:::plot_ice,
+  not_available_some_predictions_fail = FALSE,
+  not_available_all_prospective = TRUE,
+  not_available_mostly_prospective = TRUE,
+  not_available_single_sample = TRUE,
+  data_element = "ice_data",
+  n_important_features = 1L,
+  debug = debug_flag
+)
