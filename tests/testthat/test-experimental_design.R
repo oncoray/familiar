b96bf3274ae029ae23814eb555829ca7adb5f112
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-05-20 15:11:10 +0200

    Revise tests in test-experimental_design

--- tests/testthat/test-experimental_design.R
+++ tests/testthat/test-experimental_design.R
@@ -4,88 +4,100 @@ testthat::skip_on_ci()
 debug_flag <- FALSE
 
 # Simple test
-familiar:::integrated_test(
-  experimental_design = "fs+mb",
-  vimp_method = "none",
-  cluster_method = "none",
-  imputation_method = "simple",
-  parallel = FALSE,
-  skip_evaluation_elements = "all",
-  outcome_type_available = "binomial",
-  debug = debug_flag
-)
+vimp_methods <- c("none", "mim", "mim")
+experimental_designs <- c("fs+mb", "fs+mb", "mb")
+for (ii in seq_along(vimp_methods)) {
+  familiar:::integrated_test(
+    experimental_design = experimental_designs[ii],
+    vimp_method = vimp_methods[ii],
+    parallel = FALSE,
+    estimation_type = "point",
+    outcome_type_available = "binomial",
+    debug = debug_flag
+  )
+}
+
 
 # Bootstrap (without optimisation within bootstraps)
-familiar:::integrated_test(
-  experimental_design = "bt(fs+mb, 5)",
-  vimp_method = "none",
-  cluster_method = "none",
-  imputation_method = "simple",
-  parallel = FALSE,
-  skip_evaluation_elements = "all",
-  outcome_type_available = "binomial",
-  debug = debug_flag
-)
+experimental_designs <- c("bt(fs+mb, 3)", "bt(fs+mb, 3)", "bt(mb, 3)")
+for (ii in seq_along(vimp_methods)) {
+  familiar:::integrated_test(
+    experimental_design = experimental_designs[ii],
+    vimp_method = vimp_methods[ii],
+    parallel = FALSE,
+    skip_evaluation_elements = "all",
+    outcome_type_available = "binomial",
+    debug = debug_flag
+  )
+}
 
 # Bootstrap (with pre-processing and optimisation within bootstraps)
-familiar:::integrated_test(
-  experimental_design = "bs(fs+mb, 5)",
-  vimp_method = "none",
-  cluster_method = "none",
-  imputation_method = "simple",
-  parallel = FALSE,
-  skip_evaluation_elements = "all",
-  outcome_type_available = "binomial",
-  debug = debug_flag
-)
+experimental_designs <- c("bs(fs+mb, 3)", "bs(fs+mb, 3)", "bs(mb, 3)")
+for (ii in seq_along(vimp_methods)) {
+  familiar:::integrated_test(
+    experimental_design = experimental_designs[ii],
+    vimp_method = vimp_methods[ii],
+    parallel = FALSE,
+    skip_evaluation_elements = "all",
+    outcome_type_available = "binomial",
+    debug = debug_flag
+  )
+}
+
 
 # Cross-validation
-familiar:::integrated_test(
-  experimental_design = "cv(fs+mb, 3)",
-  vimp_method = "none",
-  cluster_method = "none",
-  imputation_method = "simple",
-  parallel = FALSE,
-  skip_evaluation_elements = "all",
-  outcome_type_available = "binomial",
-  debug = debug_flag
-)
+experimental_designs <- c("cv(fs+mb, 3)", "cv(fs+mb, 3)", "cv(mb, 3)")
+for (ii in seq_along(vimp_methods)) {
+  familiar:::integrated_test(
+    experimental_design = experimental_designs[ii],
+    vimp_method = vimp_methods[ii],
+    parallel = FALSE,
+    skip_evaluation_elements = "all",
+    outcome_type_available = "binomial",
+    debug = debug_flag
+  )
+}
+
 
 # Leave-one-out cross-validation
-familiar:::integrated_test(
-  experimental_design = "lv(fs+mb)",
-  vimp_method = "none",
-  cluster_method = "none",
-  imputation_method = "simple",
-  parallel = FALSE,
-  skip_evaluation_elements = "all",
-  outcome_type_available = "binomial",
-  debug = debug_flag
-)
+experimental_designs <- c("lv(fs+mb)", "lv(fs+mb)", "lv(mb)")
+for (ii in seq_along(vimp_methods)) {
+  familiar:::integrated_test(
+    experimental_design = experimental_designs[ii],
+    vimp_method = vimp_methods[ii],
+    parallel = FALSE,
+    skip_evaluation_elements = "all",
+    outcome_type_available = "binomial",
+    debug = debug_flag
+  )
+}
 
 # Imbalance corrections using full undersampling
-familiar:::integrated_test(
-  experimental_design = "ip(fs+mb)",
-  imbalance_correction_method = "full_undersampling",
-  vimp_method = "none",
-  cluster_method = "none",
-  imputation_method = "simple",
-  parallel = FALSE,
-  skip_evaluation_elements = "all",
-  outcome_type_available = "binomial",
-  debug = debug_flag
-)
+experimental_designs <- c("ip(fs+mb)", "ip(fs+mb)", "ip(mb)")
+for (ii in seq_along(vimp_methods)) {
+  familiar:::integrated_test(
+    experimental_design = experimental_designs[ii],
+    imbalance_correction_method = "full_undersampling",
+    vimp_method = vimp_methods[ii],
+    parallel = FALSE,
+    skip_evaluation_elements = "all",
+    outcome_type_available = "binomial",
+    debug = debug_flag
+  )
+}
+
 
 # Imbalance corrections using full undersampling
-familiar:::integrated_test(
-  experimental_design = "ip(fs+mb)",
-  imbalance_correction_method = "random_undersampling",
-  imbalance_n_partitions = 3,
-  vimp_method = "none",
-  cluster_method = "none",
-  imputation_method = "simple",
-  parallel = FALSE,
-  skip_evaluation_elements = "all",
-  outcome_type_available = "binomial",
-  debug = debug_flag
-)
+experimental_designs <- c("ip(fs+mb)", "ip(fs+mb)", "ip(mb)")
+for (ii in seq_along(vimp_methods)) {
+  familiar:::integrated_test(
+    experimental_design = experimental_designs[ii],
+    imbalance_correction_method = "random_undersampling",
+    imbalance_n_partitions = 3,
+    vimp_method = vimp_methods[ii],
+    parallel = FALSE,
+    skip_evaluation_elements = "all",
+    outcome_type_available = "binomial",
+    debug = debug_flag
+  )
+}
