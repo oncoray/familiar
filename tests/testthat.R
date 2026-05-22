1e43b6654d2d6efdb50ae8e36d9b5e763dd14ee8
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-05-22 20:18:28 +0200

    Limit CPU on CRAN.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,15 @@
 library(testthat)
 library(familiar)
 
+on_cran <- function() {
+  !interactive() && identical(Sys.getenv("NOT_CRAN"), "")
+}
+
+# Prevent thread overuse (through data.table?) when running tests on CRAN.
+if (on_cran()) {
+  Sys.setenv("OMP_THREAD_LIMIT" = 2L)
+}
+
 suppressWarnings(
   testthat::test_check("familiar"),
   classes = c("deprecation_warning")
