42fc0d16f8f4df3f2e24770e6be2dc3e49e41fa0
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-05-22 17:31:43 +0200

    Evaluation makes this test take a long time.

--- tests/testthat/test-experimental_design_loocv.R
+++ tests/testthat/test-experimental_design_loocv.R
@@ -8,7 +8,7 @@ familiar:::integrated_test(
   experimental_design = "lv(fs+mb)",
   vimp_method = "none",
   parallel = FALSE,
-  estimation_type = "point",
+  skip_evaluation_elements = "all",
   outcome_type_available = "binomial",
   debug = debug_flag
 )
