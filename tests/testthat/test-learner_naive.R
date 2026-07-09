81ed86f7c8ff4359f138c8e782183cf004d82a2f
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-07-09 13:09:57 +0200

    Add tests for naive learners.

---
+++ tests/testthat/test-learner_naive.R
@@ -0,0 +1,26 @@
+# First test if all selectable learners are also available
+familiar:::test_all_learners_available(
+  learners = familiar:::.get_available_naive_learn(show_general = TRUE)
+)
+
+# Don't perform any further tests on CRAN due to time of running the complete
+# test.
+testthat::skip_on_cran()
+testthat::skip_on_ci()
+
+familiar:::test_all_learners_train_predict_vimp(
+  learners = familiar:::.get_available_naive_learn(show_general = FALSE),
+  has_vimp = FALSE
+)
+
+familiar:::test_all_learners_parallel_train_predict_vimp(
+  learners = familiar:::.get_available_naive_learn(show_general = FALSE),
+  has_vimp = FALSE
+)
+
+testthat::skip("Skip hyperparameter optimisation, unless manual.")
+
+familiar:::test_hyperparameter_optimisation(
+  learners = familiar:::.get_available_naive_learn(show_general = TRUE),
+  debug = TRUE
+)
