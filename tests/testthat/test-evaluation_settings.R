29acea7b7a8ab3291cef3eb7395781dbc0f4614a
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-08-12 17:13:01 +0200

    Add test for detail_level
    #104

--- tests/testthat/test-evaluation_settings.R
+++ tests/testthat/test-evaluation_settings.R
@@ -10,11 +10,13 @@ testthat::test_that(
     settings <- familiar:::.create_test_settings()
     testthat::expect_null(settings$eval$sample_limit)
     
+    # Single value for all relevant data elements.
     settings <- familiar:::.create_test_settings(sample_limit = 30L)
     for (x in familiar:::.get_available_data_elements(check_has_sample_limit = TRUE)) {
       testthat::expect_equal(settings$eval$sample_limit[[x]], 30L)
     }
     
+    # Different values for specifc data elements.
     settings <- familiar:::.create_test_settings(
       sample_limit = list(
         "permutation_vimp" = 30L,
@@ -36,11 +38,13 @@ testthat::test_that(
     settings <- familiar:::.create_test_settings()
     testthat::expect_null(settings$eval$n_important_features)
     
+    # Single value for all relevant data elements.
     settings <- familiar:::.create_test_settings(n_important_features = 30L)
     for (x in familiar:::.get_available_data_elements(check_has_n_important_features = TRUE)) {
       testthat::expect_equal(settings$eval$n_important_features[[x]], 30L)
     }
     
+    # Different values for specifc data elements.
     settings <- familiar:::.create_test_settings(
       n_important_features = list(
         "permutation_vimp" = 30L,
@@ -53,6 +57,34 @@ testthat::test_that(
   }
 )
 
+
 # detail_level -----------------------------------------------------------------
+testthat::test_that(
+  "detail_level is correctly parsed",
+  {
+    # Unset by default
+    settings <- familiar:::.create_test_settings()
+    testthat::expect_null(settings$eval$detail_level)
+    
+    # Single value for all relevant data elements.
+    settings <- familiar:::.create_test_settings(detail_level = "ensemble")
+    for (x in familiar:::.get_available_data_elements(check_has_detail_level = TRUE)) {
+      testthat::expect_equal(settings$eval$detail_level[[x]], "ensemble")
+    }
+    
+    # Different values for specifc data elements.
+    settings <- familiar:::.create_test_settings(
+      detail_level = list(
+        "permutation_vimp" = "hybrid",
+        "ice_data" = "model"
+      )
+    )
+    testthat::expect_equal(settings$eval$detail_level$permutation_vimp, "hybrid")
+    testthat::expect_equal(settings$eval$detail_level$ice_data, "model")
+    testthat::expect_null(settings$eval$detail_level$shap)
+  }
+)
+
+
 # estimation_type --------------------------------------------------------------
 # aggregate_results ------------------------------------------------------------
