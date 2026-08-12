b45e170d05be4aaf863f5504b2226c97d972cae8
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-08-12 17:09:36 +0200

    Add tests for n_important_features.
    #104

--- tests/testthat/test-evaluation_settings.R
+++ tests/testthat/test-evaluation_settings.R
@@ -23,13 +23,36 @@ testthat::test_that(
     )
     testthat::expect_equal(settings$eval$sample_limit$permutation_vimp, 30L)
     testthat::expect_equal(settings$eval$sample_limit$ice_data, 25L)
-    testthat::expect_null(settings$eval$shap)
+    testthat::expect_null(settings$eval$sample_limit$shap)
   }
 )
 
 
-
 # n_important_features ---------------------------------------------------------
+testthat::test_that(
+  "n_important_features is correctly parsed",
+  {
+    # Unset by default
+    settings <- familiar:::.create_test_settings()
+    testthat::expect_null(settings$eval$n_important_features)
+    
+    settings <- familiar:::.create_test_settings(n_important_features = 30L)
+    for (x in familiar:::.get_available_data_elements(check_has_n_important_features = TRUE)) {
+      testthat::expect_equal(settings$eval$n_important_features[[x]], 30L)
+    }
+    
+    settings <- familiar:::.create_test_settings(
+      n_important_features = list(
+        "permutation_vimp" = 30L,
+        "ice_data" = 25L
+      )
+    )
+    testthat::expect_equal(settings$eval$n_important_features$permutation_vimp, 30L)
+    testthat::expect_equal(settings$eval$n_important_features$ice_data, 25L)
+    testthat::expect_null(settings$eval$n_important_features$shap)
+  }
+)
+
 # detail_level -----------------------------------------------------------------
 # estimation_type --------------------------------------------------------------
 # aggregate_results ------------------------------------------------------------
