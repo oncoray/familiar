88af858c9bbcb3dd54202e313cf79449f9a41bc7
Alex Zwanenburg <alexander.zwanenburg@nct-dresden.de>
2026-08-19 16:46:40 +0200

    Add allow_naive_models parameter and documentation.
    #101

--- R/ParseSettings.R
+++ R/ParseSettings.R
@@ -2706,6 +2706,15 @@
 #' @param parallel Logical value that whether familiar uses parallelisation. If
 #'   `FALSE` it will override `parallel_hyperparameter_optimisation`.
 #' @param outcome_type Type of outcome found in the data set.
+#' @param allow_naive_models (*optional*) Enables training of naive models in
+#'   case none of the actual models perform better than random. Naive models
+#'   predict the least informative value, e.g. the most common class for binomial
+#'   and multinomial problems, or the median value for regression problems.
+#'   
+#'   If `FALSE`, the least worst set of hyperparameters is used to train the model.
+#'   
+#'   By default, naive models are trained when no better set of hyperparameters 
+#'   can be identified (`TRUE`).
 #' @param optimisation_bootstraps (*optional*) Number of bootstraps that should
 #'   be generated from the development data set. During the optimisation
 #'   procedure one or more of these bootstraps (indicated by
@@ -3002,6 +3011,7 @@
     config = NULL,
     parallel,
     outcome_type,
+    allow_naive_models = waiver(),
     optimisation_bootstraps = waiver(),
     optimisation_determine_vimp = waiver(),
     smbo_random_initialisation = waiver(),
@@ -3069,6 +3079,16 @@
     default = TRUE
   )
 
+  # allow_naive_models ---------------------------------------------------------
+  settings$hpo_allow_naive_models <- .parse_arg(
+    x_config = config$allow_naive_models,
+    x_var = allow_naive_models,
+    var_name = "allow_naive_models",
+    type = "logical",
+    optional = TRUE,
+    default = TRUE
+  )
+  
   # max_smbo_iterations --------------------------------------------------------
   # Maximum number of SMBO iterations before stopping
   settings$hpo_smbo_iter_max <- .parse_arg(
